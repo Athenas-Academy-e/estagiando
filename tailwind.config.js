386f8a75646f39f711455a6662914f76650a588e
caiofavero <caiofavero@coisalab.com>
2025-10-18 20:03:37 -0300

    enabled tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,php,js}",
+    "./templates/**/*.{html,php,js}",
+    "./inc/**/*.{html,php,js}",
+    "./*.{html,php}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
