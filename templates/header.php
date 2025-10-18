f88b4b33ada422c4c8af7997ecae51a8bdb92b41
caiofavero <caiofavero@coisalab.com>
2025-10-18 09:26:24 -0300

    create unique header

--- templates/header.php
+++ templates/header.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-<meta charset="UTF-8">
-<title>Estagiando</title>
-<script src="https://cdn.tailwindcss.com"></script>
-</head>
+<?php include_once('../templates/head.php');?>
 <body class="bg-gray-50 font-sans min-h-screen p-4 md:p-8">
 <header class="max-w-6xl mx-auto mb-6 flex justify-between items-center">
   <div>
     <h1 class="text-3xl font-bold">Quadro de Vagas</h1>
-    <p class="text-gray-600 text-sm">PHP + MySQL + Tailwind + Modal de candidatura</p>
   </div>
   <a href="/public/add.php" class="bg-blue-600 text-white px-3 py-2 rounded-md shadow hover:bg-blue-700">Publicar vaga</a>
 </header>
-<main class="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-6">
+
